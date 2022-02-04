196aa787e86e78eb312e89769e6fc364c2f2eac9
manichandra-github <72243493+manichandra-github@users.noreply.github.com>
2022-02-04 20:36:03 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who is Tony Stark father?",
+    answer: "Howard Stark",
+    options: [
+      "Howard Stark",
+      "Morgan Stark",
+      "Maria Stark",
+      "Howard Stank"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Hulk Name?",
+    answer: "Bruce Banner",
+    options: [
+      "Chris",
+      "Anthony",
+      "Steve Banner",
+      "Bruce Banner"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does EDITH stand for?",
+    answer: "Even Dead Im the Hero",
+    options: [
+      "Even Dead Im the Hero",
+      "Even Death Im Alive",
+      "Even Dead Im the Hulk",
+      "Even Death Im in the Home"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Deadpool Actor Name?",
+    answer: "Ryan Reynolds",
+    options: [
+      "Ryan Reynolds",
+      "Rodney Reynolds",
+      "Wade Wilson",
+      "Wade Reynolds"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is Villain in Endgame ?",
+    answer: "Thanos",
+    options: [
+      "Thanos",
+      "Star Lord",
+      "Galactus",
+      "Kang - The Conqueror"
+    ]
+  },
+];
